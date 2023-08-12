a3dc78ec7528c4bc20132196ed4827492dc2bef0
Kevin Dethoor <k.dethoor@gmail.com>
2023-08-12 13:51:34 +0900

    [Neovim] Extend remaps

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,2 +1,26 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Do not move the cursor when appending the line below at the end of the current line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep the cursor in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Do not replace the yanked text with the replaced text when pasting
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Replace word under cursor in file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
