2447175c9c95ef41d3be39bf0d4c626d35a724fd
Kevin Dethoor <k.dethoor@gmail.com>
2023-08-12 13:49:55 +0900

    [Neovim] Configure editor

--- nvim/lua/core/editor.lua
+++ nvim/lua/core/editor.lua
@@ -1,3 +1,34 @@
-vim.wo.relativenumber = true
-vim.g.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
+-- Leader key
+vim.g.mapleader = " "
 
+-- Colors
+vim.opt.termguicolors = true
+
+-- Indenting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.opt.smartindent = true
+
+-- Line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+vim.g.netrw_bufsettings = "noma nomod rnu nowrap ro nobl"
+
+-- Line wrapping
+vim.opt.wrap = false
+
+-- Column hint
+vim.opt.colorcolumn = "88"
+
+-- Search
+vim.opt.incsearch = true
+
+-- Scrolling
+vim.opt.scrolloff = 8
+
+-- Swap and backup files
+vim.opt.swapfile = false
+vim.opt.backup = false
