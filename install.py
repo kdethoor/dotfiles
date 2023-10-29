0fbe062cd97388e360480fa80f6700184585900d
Kevin Dethoor <k.dethoor@gmail.com>
2023-10-29 13:34:22 +0900

    Finish install script for Neovim

--- install.py
+++ install.py
@@ -4,6 +4,19 @@ from enum import Enum, IntEnum
 import os
 from pathlib import Path
 import sys
+import traceback
+
+
+class Dotfiles:
+    NEOVIM_DIR_NAME = "nvim"
+
+    @staticmethod
+    def get_root() -> Path:
+        return Path(__file__).parent
+
+    @classmethod
+    def get_neovim_dir(cls) -> Path:
+        return cls.get_root() / cls.NEOVIM_DIR_NAME
 
 
 class Platform(Enum):
@@ -19,15 +32,20 @@ class VerboseLevel(IntEnum):
     Trace = 3
 
 
-NEOVIM_INSTALL_DIR: dict[Platform, Path] = {
-    Platform.Linux: Path(os.environ["HOME"]) / ".config" / "nvim",
-    Platform.Windows: Path(os.environ["LOCALAPPDATA"]) / "nvim",
-}
-
-
 class InstallFailedError(Exception):
     def __init__(self, reason):
         self.message = f"Failed installation because: {reason}"
+
+
+class Neovim:
+    @staticmethod
+    def get_install_dir(platform: Platform) -> Path | None:
+        if platform == Platform.Linux:
+            return Path(os.environ["HOME"]) / ".config" / "nvim"
+        elif platform == Platform.Windows:
+            return Path(os.environ["LOCALAPPDATA"]) / "nvim"
+        else:
+            return None
 
 
 def get_platform() -> Platform:
@@ -39,10 +57,6 @@ def get_platform() -> Platform:
         p = Platform.Windows
 
     return p
-
-
-def get_dotfile_root():
-    return Path(__file__).parent
 
 
 def get_backup_path(path) -> Path:
@@ -61,7 +75,7 @@ def install(
     src: Path, dest: Path, with_backups: bool, is_dir: bool, verbose=VerboseLevel.Info
 ):
     if not src.exists():
-        raise InstallFailedError(f"{str(src)} does not exists")
+        raise InstallFailedError(f"{str(src)} does not exist")
 
     if dest.exists():
         if with_backups:
@@ -69,36 +83,55 @@ def install(
             dest.rename(backup)
             if verbose >= VerboseLevel.Trace:
                 print(f"Backup'ed {str(dest)} -> {str(backup)}")
-    src.symlink_to(dest, target_is_directory=is_dir)
+        else:
+            raise InstallFailedError(
+                f"Neovim config already exists at {str(dest)} but backups are disabled"
+            )
+    dest.symlink_to(src, target_is_directory=is_dir)
     if verbose >= VerboseLevel.Info:
         print(f"Installed {str(src)} -> {str(dest)}")
 
 
 def install_neovim(with_backups: bool):
     platform = get_platform()
-    src: Path | None = None
-    dest: Path | None = None
-    if src is None or dest is None:
+    src = Dotfiles.get_neovim_dir()
+    dest = Neovim.get_install_dir(platform)
+    if dest is None:
         raise InstallFailedError(
-            f"Installation settings could be determine for platform {platform}"
+            f"Neovim installation directory could not be determined for platform {platform}"
         )
-    src = get_dotfile_root() / "neovim"
-    dest = NEOVIM_INSTALL_DIR[platform]
     try:
-        install(src, dest, with_backups, is_dir=True)
+        install(src, dest, with_backups, True)
     except Exception as e:
         raise InstallFailedError("Failed installing Neovim") from e
 
 
-def ask_for_backups() -> bool:
-    return True
+def ask_for_backups(verbose=VerboseLevel.Info) -> bool:
+    with_backups: None | bool = None
+    while with_backups is None:
+        answer = input("Backup the existing configurations before each install? [Y|n] ")
+        if answer in ["Y", "y", ""]:
+            with_backups = True
+        elif answer in ["N", "n"]:
+            with_backups = False
+    if verbose >= VerboseLevel.Info:
+        print(
+            f"Existing configurations will{' ' if with_backups else ' not '}be backup'ed before each install"
+        )
+    print()
+    return with_backups
 
 
-def main():
-    with_backups = ask_for_backups
-    pass
-    #todo: add install calls
+def main() -> int:
+    with_backups = ask_for_backups()
+    try:
+        install_neovim(with_backups)
+    except Exception:
+        traceback.print_exc(file=sys.stderr)
+        return 1
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    res = main()
+    sys.exit(res)
