cdfbaf112d6ab7e9cf62d24b2777ced9a29359fa
Kevin Dethoor <k.dethoor@gmail.com>
2024-08-13 06:18:06 +0900

    [Neovim] Disable plugins when Neovim is used by VSCode

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -9,31 +9,45 @@ local ensure_packer = function()
   return false
 end
 
+local is_only_enabled_outside_code = function()
+	return vim.g.vscode == nil
+end
+
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
 	-- Packer
-	use "wbthomason/packer.nvim"
+	use({"wbthomason/packer.nvim"})
 	-- Other plugins
 	use({
 		"nvim-telescope/telescope.nvim", tag = "0.1.2",
-		requires = { {"nvim-lua/plenary.nvim"} }
+		cond = is_only_enabled_outside_code(),
+		requires = { {"nvim-lua/plenary.nvim"} },
 	})
 	use({
 		"catppuccin/nvim",
+		cond = is_only_enabled_outside_code(),
 		as = "catppuccin",
 	})
 	use({
 		"nvim-treesitter/nvim-treesitter",
+		cond = is_only_enabled_outside_code(),
 		run = function()
 			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
 	})
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
+	use({
+		"mbbill/undotree",
+		cond = is_only_enabled_outside_code(),
+	})
+	use({
+		"tpope/vim-fugitive",
+		cond = is_only_enabled_outside_code(),
+	})
 	use({
 		'VonHeikemen/lsp-zero.nvim',
+		cond = is_only_enabled_outside_code(),
 		branch = 'v3.x',
 		requires = {
 			-- LSP Support
@@ -45,15 +59,23 @@ return require("packer").startup(function(use)
 			{'hrsh7th/nvim-cmp'},     -- Required
 			{'hrsh7th/cmp-nvim-lsp'}, -- Required
 			{'L3MON4D3/LuaSnip'},     -- Required
-		}
+		},
 	})
 	use({"stevearc/conform.nvim",
+		cond = is_only_enabled_outside_code(),
 		config = function()
 			require("conform").setup()
 		end
 	})
-	use({"mfussenegger/nvim-dap"})
-	use({"rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}})
+	use({
+		"mfussenegger/nvim-dap",
+		cond = is_only_enabled_outside_code(),
+	})
+	use({
+		"rcarriga/nvim-dap-ui",
+		cond = is_only_enabled_outside_code(),
+		requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+	})
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
